35d063e706e486038e4ab0c73f5080f298cc6ede
Fábio Sales <fabio.oliveira.sales@gmail.com>
2024-03-13 10:46:16 -0300

    separando os script.

--- book-room/quarto/src/main/resources/db/changelog/changes/postgres/V3__CARGA_DADO_TESTE.sql
+++ book-room/quarto/src/main/resources/db/changelog/changes/postgres/V3__CARGA_DADO_TESTE.sql
@@ -1,26 +1,7 @@
--- Inserir informações sobre localidade
-INSERT INTO book_room_quarto.localidade (nome, rua_av, numero, cep, cidade, estado)
-VALUES ('Fazenda da Pós Tech', 'Rua da Pós Tech', '567', '01000-002', 'Cidade da Pós Tech', 'Estado da Pós Tech');
 
 -- Inserir informações sobre prédio
 INSERT INTO book_room_quarto.predio (id_localidade, nome)
 VALUES (1, 'Casa Principal');
-
--- Inserir informações sobre tipos de quartos
-INSERT INTO book_room_quarto.tipo (nome, categoria)
-VALUES
-    ('Standard Simples', 'Quarto'),
-    ('Standard Duplo', 'Quarto'),
-    ('Luxo Simples', 'Quarto'),
-    ('Luxo Duplo', 'Quarto'),
-    ('Premium Simples', 'Quarto'),
-    ('Premium Duplo', 'Quarto'),
-    ('Simples', 'Banheiro'),
-    ('Luxo', 'Banheiro'),
-    ('Premium', 'Banheiro'),
-    ('Queen Size','Cama'),
-    ('Solteiro','Cama'),
-    ('King Size','Cama');
 
 -- Inserir informações sobre quartos
 INSERT INTO book_room_quarto.quarto (id_predio, id_localidade, total_pessoas, tipo, valor_diaria, quantidade_quartos)
@@ -83,27 +64,3 @@ VALUES
     (4, 11, 2, 'Solteiro'),
     (5, 12, 1, 'King Size'),
     (6, 12, 2, 'King Size');
-
--- Inserir informações sobre amenidades
-INSERT INTO book_room_quarto.amenidades (descricao)
-VALUES
-    ('Piscina Adulto, aquecida e coberta'),
-    ('Piscina Adulto, não aquecida em área aberta'),
-    ('Piscina Infantil, aquecida e coberta'),
-    ('Piscina Infantil, não aquecida em área aberta'),
-    ('Restaurantes no estilo Self Service'),
-    ('Restaurante a La Carte'),
-    ('Área Kids, com brinquedoteca, vídeo games e biblioteca'),
-    ('Equipe de Entretenimento Infantil e adulto');
-
--- Inserir associações de amenidades para a localidade
-INSERT INTO book_room_quarto.amenidades_localidade (id_localidade, id_amenidade, quantidade)
-VALUES
-    (1, 1, 1), -- Piscina Adulto, aquecida e coberta
-    (1, 2, 1), -- Piscina Adulto, não aquecida em área aberta
-    (1, 3, 1), -- Piscina Infantil, aquecida e coberta
-    (1, 4, 1), -- Piscina Infantil, não aquecida em área aberta
-    (1, 5, 2), -- Restaurantes no estilo Self Service
-    (1, 6, 1), -- Restaurante a La Carte
-    (1, 7, 1), -- Área Kids, com brinquedoteca, vídeo games e biblioteca
-    (1, 8, 2); -- Equipe de Entretenimento Infantil e adulto
