652eb2ac5582b3ac31fdf158fefc2dd1b57f9a4b
Fábio Sales <fabio.oliveira.sales@gmail.com>
2024-03-14 02:49:10 -0300

    criando test

--- book-room/quarto/src/test/java/br/com/book/room/quarto/bdd/AmenidadesSteps.java
+++ book-room/quarto/src/test/java/br/com/book/room/quarto/bdd/AmenidadesSteps.java
@@ -1,24 +1,47 @@
 package br.com.book.room.quarto.bdd;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import io.cucumber.java.Before;
 import io.cucumber.java.pt.Então;
 import io.cucumber.java.pt.Quando;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AmenidadesSteps {
 
-    @Quando("faço uma requisição para listar todas as amenidades")
-    public void faço_uma_requisição_para_listar_todas_as_amenidades() {
-//        // Write code here that turns the phrase above into concrete actions
-//        throw new io.cucumber.java.PendingException();
-    }
-    @Então("deve ser retornado o status code {int}")
-    public void deve_ser_retornado_o_status_code(Integer int1) {
-//        // Write code here that turns the phrase above into concrete actions
-//        throw new io.cucumber.java.PendingException();
-    }
+	@LocalServerPort
+	private int port;
 
-    @Então("deve ser retornado todas as amenidades cadastradas")
-    public void deve_ser_retornado_todas_as_amenidades_cadastradas() {
-//        // Write code here that turns the phrase above into concrete actions
-//        throw new io.cucumber.java.PendingException();
-    }
+	private Response response;
+	private final static String BASE_URI = "http://localhost";
+
+
+	@Before
+	public void setup() throws Exception {
+		RestAssured.baseURI = BASE_URI;
+		RestAssured.port = port;
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+	}
+
+	@Quando("faço uma requisição para listar todas as amenidades")
+	public void faço_uma_requisição_para_listar_todas_as_amenidades() {
+		response = RestAssured.get("/amenidades");
+	}
+
+	@Então("deve ser retornado o status code {int}")
+	public void deve_ser_retornado_o_status_code(Integer statusCode) {
+		assertEquals(statusCode, response.getStatusCode());
+	}
+
+	@Então("deve ser retornado todas as amenidades cadastradas")
+	public void deve_ser_retornado_todas_as_amenidades_cadastradas() {
+		assertEquals(200, response.getStatusCode());
+	}
+
 }
