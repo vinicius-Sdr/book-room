ec425f1dbae99f3f53750594e7e8e206eecd9ac5
Fábio Sales <fabio.oliveira.sales@gmail.com>
2024-03-14 00:04:59 -0300

    criando servico de amenidades

--- book-room/quarto/src/main/java/br/com/book/room/quarto/infrastructure/config/swagger/SpringDocOpenApiConfig.java
+++ book-room/quarto/src/main/java/br/com/book/room/quarto/infrastructure/config/swagger/SpringDocOpenApiConfig.java
@@ -3,7 +3,6 @@ package br.com.book.room.quarto.infrastructure.config.swagger;
 import br.com.book.room.quarto.infrastructure.config.swagger.properties.SwaggerConfigProperties;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
@@ -24,14 +23,12 @@ public class SpringDocOpenApiConfig {
 	@Bean
 	public OpenAPI openAPI() {
 		var app = swaggerConfigProperties.getApplication();
-		var info = new Info().title(app.title())
+
+		Info info = new Info().title(app.title())
 			.description(app.description())
 			.version(app.version())
-			.contact(new Contact().name("Fábio de Oliveira Sales")
-				.email("fabio.oliveira.sales@gmail.com")
-				.name("Vinicius Santana dos Reis")
-				.email(""))
 			.license(new License().name("MIT License"));
+
 		return new OpenAPI().components(new Components()).info(info);
 	}
 
